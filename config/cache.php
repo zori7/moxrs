9523e3aa5f9f54a853dd93e64552ad7738f755eb
David Martirosyan <david1994martirosyan@gmail.com>
2019-06-26 14:13:35 +0400

    conflict fix

--- config/cache.php
+++ config/cache.php
@@ -54,17 +54,9 @@ return [
 
         'memcached' => [
             'driver' => 'memcached',
-            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-            'sasl' => [
-                env('MEMCACHED_USERNAME'),
-                env('MEMCACHED_PASSWORD'),
-            ],
-            'options' => [
-                // Memcached::OPT_CONNECT_TIMEOUT => 2000,
-            ],
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'host' => env('MEMCACHED_HOST', 'localhost'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1 v-if="currentTime">{{ currentTime.toLocaleTimeString() }}</h1>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1 v-if="currentTime">{{ currentTime.toLocaleTimeString() }}</h1>
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'home',
-  data () {
-    return {
-      currentTime: null
+    export default {
+        name: 'home',
+        data() {
+            return {
+                currentTime: null
+            }
+        },
+        mounted() {
+            this.getTime();
+            setInterval(this.getTime, 1000);
+        },
+        methods: {
+            getTime() {
+                axios.get('api/time').then(res => {
+                    this.currentTime = new Date(res.data.time);
+                })
+            }
+        }
     }
-  },
-  mounted () {
-    axios.get('api/time').then(res => {
-      this.currentTime = new Date(res.data.time)
-    })
-  }
-}
 </script>
